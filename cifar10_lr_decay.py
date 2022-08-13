817f888f58993f44eb6ae51c59e8630e110ae74d
Baishali Dutta <me@itsbaishali.com>
2022-08-13 07:49:10 +0200

    Reformatted Application File

--- cifar10_lr_decay.py
+++ cifar10_lr_decay.py
@@ -8,6 +8,7 @@ __version__ = "0.1"
 
 # set the matplotlib backend so figures can be saved in the background
 import matplotlib
+
 matplotlib.use("Agg")
 
 # import the necessary packages
@@ -21,23 +22,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 
+
 def step_decay(epoch):
-	# initialize the base initial learning rate, drop factor, and
-	# epochs to drop every
-	initAlpha = 0.01
-	factor = 0.25
-	dropEvery = 5
+    # initialize the base initial learning rate, drop factor, and
+    # epochs to drop every
+    initAlpha = 0.01
+    factor = 0.25
+    dropEvery = 5
 
-	# compute learning rate for the current epoch
-	alpha = initAlpha * (factor ** np.floor((1 + epoch) / dropEvery))
+    # compute learning rate for the current epoch
+    alpha = initAlpha * (factor ** np.floor((1 + epoch) / dropEvery))
 
-	# return the learning rate
-	return float(alpha)
+    # return the learning rate
+    return float(alpha)
+
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-o", "--output", required=True,
-	help="path to the output loss/accuracy plot")
+                help="path to the output loss/accuracy plot")
 args = vars(ap.parse_args())
 
 # load the training and testing data, then scale it into the
@@ -54,7 +57,7 @@ testY = lb.transform(testY)
 
 # initialize the label names for the CIFAR-10 dataset
 labelNames = ["airplane", "automobile", "bird", "cat", "deer",
-	"dog", "frog", "horse", "ship", "truck"]
+              "dog", "frog", "horse", "ship", "truck"]
 
 # define the set of callbacks to be passed to the model during
 # training
@@ -64,17 +67,17 @@ callbacks = [LearningRateScheduler(step_decay)]
 opt = SGD(lr=0.01, momentum=0.9, nesterov=True)
 model = MiniVGGNet.build(width=32, height=32, depth=3, classes=10)
 model.compile(loss="categorical_crossentropy", optimizer=opt,
-	metrics=["accuracy"])
+              metrics=["accuracy"])
 
 # train the network
 H = model.fit(trainX, trainY, validation_data=(testX, testY),
-	batch_size=64, epochs=40, callbacks=callbacks, verbose=1)
+              batch_size=64, epochs=40, callbacks=callbacks, verbose=1)
 
 # evaluate the network
 print("[INFO] evaluating network...")
 predictions = model.predict(testX, batch_size=64)
 print(classification_report(testY.argmax(axis=1),
-	predictions.argmax(axis=1), target_names=labelNames))
+                            predictions.argmax(axis=1), target_names=labelNames))
 
 # plot the training loss and accuracy
 plt.style.use("ggplot")
