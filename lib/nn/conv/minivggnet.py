ff5b6c9c55041826747d51a1b728d5a5891e9419
Baishali Dutta <me@itsbaishali.com>
2022-08-18 07:54:23 +0200

    Updated var names in minivggnet

--- lib/nn/conv/minivggnet.py
+++ lib/nn/conv/minivggnet.py
@@ -16,32 +16,32 @@ class MiniVGGNet:
         # "channels last" and the channels dimension itself
         model = Sequential()
         input_shape = (height, width, depth)
-        chanDim = -1
+        chan_dim = -1
 
         # if we are using "channels first", update the input shape
         # and channels dimension
         if K.image_data_format() == "channels_first":
-            inputShape = (depth, height, width)
-            chanDim = 1
+            input_shape = (depth, height, width)
+            chan_dim = 1
 
         # first CONV => RELU => CONV => RELU => POOL layer set
         model.add(Conv2D(32, (3, 3), padding="same",
                          input_shape=input_shape))
         model.add(Activation("relu"))
-        model.add(BatchNormalization(axis=chanDim))
+        model.add(BatchNormalization(axis=chan_dim))
         model.add(Conv2D(32, (3, 3), padding="same"))
         model.add(Activation("relu"))
-        model.add(BatchNormalization(axis=chanDim))
+        model.add(BatchNormalization(axis=chan_dim))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.25))
 
         # second CONV => RELU => CONV => RELU => POOL layer set
         model.add(Conv2D(64, (3, 3), padding="same"))
         model.add(Activation("relu"))
-        model.add(BatchNormalization(axis=chanDim))
+        model.add(BatchNormalization(axis=chan_dim))
         model.add(Conv2D(64, (3, 3), padding="same"))
         model.add(Activation("relu"))
-        model.add(BatchNormalization(axis=chanDim))
+        model.add(BatchNormalization(axis=chan_dim))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.25))
 
