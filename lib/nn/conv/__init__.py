a2ee2e98159e0cf72b4324efed1ff7ad42f66362
Baishali Dutta <me@itsbaishali.com>
2022-09-21 10:28:13 +0200

    Removed unnecessary import from conv module

--- lib/nn/conv/__init__.py
+++ lib/nn/conv/__init__.py
@@ -2,8 +2,3 @@ __author__ = "Baishali Dutta"
 __copyright__ = "Copyright (C) 2022 Baishali Dutta"
 __license__ = "Apache License 2.0"
 __version__ = "0.1"
-
-from .lenet import LeNet
-from .minivggnet import MiniVGGNet
-# import the necessary packages
-from .shallownet import ShallowNet
